6a6377bd02f6320a4a6e2c9b91d948a83f3987d8
ANS <36783010+answebdev@users.noreply.github.com>
2023-10-08 11:55:54 -0700

    update code

--- api/episodes.js
+++ api/episodes.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const items = require('./db.json');
+const episodes = require('./db.json');
 
 // Get Episodes
 router.get('/', async (req, res) => {
     try {
-        res.json(items);
+        res.json(episodes);
     } catch (error) {
         console.log(error);
         return res.status(500).send('Server error');
     }
-});
-
-// Get by episode id
-router.get(`/:id`, (req, res) => {
-    let items = episodes;
-    let data = items.find((item) => item.id == req.params.id);
-    res.json(data);
 });
 
 module.exports = router;
