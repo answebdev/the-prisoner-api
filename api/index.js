ffbb2eccd09e60cd3ded0230c306482d91c5528f
ANS <36783010+answebdev@users.noreply.github.com>
2023-10-08 14:51:50 -0700

    add rate limit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const base = require('../routes/base');
 const episodes = require('../routes/episodes');
+
 const app = require('express')();
 
-const cors = require('cors');
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 24 hours
+    max: 10000,
+});
+
+app.use(limiter);
 
 app.use(cors());
 
@@ -11,4 +20,4 @@ const PORT = process.env.PORT || 5000;
 app.use('/', base);
 app.use('/episodes', episodes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
